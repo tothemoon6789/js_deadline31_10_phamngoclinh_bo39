4f2fa2353e759697d1b05c5ea42037cda8d64c2d
tothemoon6789 <pnlinh6789@gmail.com>
2022-10-31 10:23:23 +0700

    done

--- main.js
+++ main.js
@@ -1,17 +1,55 @@
+// ---------------SET LAYOUT-------------------
+var section = document.getElementsByTagName("section");
+var li = document.getElementsByTagName("li");
+const showAnswer = (active) => {
+    for (let i = 0; i < section.length; i++) {
+        const element = section[i];
+        if (i===active) {
+            element.setAttribute("style","display:block");
+        }else {
+            element.setAttribute("style","display:none");
+        }
+        
+    }
+    for (let i = 0; i < li.length; i++) {
+        const element = li[i];
+        if (i===active) {
+            element.setAttribute("class","active");
+        }else{
+            element.setAttribute("class","");
+            
+        }
+        
+    }
+}
+for (let i = 0; i < li.length; i++) {
+    const element = li[i];
+    element.onclick = () => {
+        
+        showAnswer(i);
+    }
+
+}
+// ---------------END SET LAYOUT-------------------
+
 /**
  * BAI 1. Tìm số nguyên dương nhỏ nhất sao cho: 1 + 2 + … + n > 10000
  */
 // input
 var sNumber = 0;// Smalest number.
+var sNumber_btn = document.getElementById("sNumber_btn");
 // end input
 // handel
-for (let i = 0; i < 10000; i++) {
-    sNumber += i;
-    if (sNumber >= 10000) {
-        // output
-        sNumber = i;
-        // end output
-        break;
+sNumber_btn.onclick = () => {
+    for (let i = 1; i < 10000; i++) {
+        sNumber += i;
+        if (sNumber >= 10000) {
+            // output
+            sNumber = i;
+            sNumber_btn.innerHTML = `Số nhỏ nhất là: ${sNumber}`;
+            // end output
+            break;
+        }
     }
 }
 // end handel
@@ -23,39 +61,64 @@ console.log(sNumber);
  * S(n) = x + x^2 + x^3 + … + x^n (Sử dụng vòng lặp và hàm)
  */
 // input
-var x = 3;
-var n = 3;
-var total = 0;
+var x = document.getElementById("totalX");
+var n = document.getElementById("totalY");
+var X = 0;
+var N = 0;
+var total_btn = document.getElementById("total_btn");
+
 // end input
 // handel
-const calTotal = (x, n) => {
-    for (let i = 1; i <= n; i++) {
-        total += Math.pow(x, i);
+total_btn.onclick = () => {
+    var total = 0;
+    X = x.value * 1;
+    N = n.value * 1;
+    if (X > 0 && N > 0) {
+        for (let i = 1; i <= N; i++) {
+            total += Math.pow(X, i);
+        }
+        // output
+        total_btn.innerHTML = `Kết quả là: ${total}`;
+        // end output
+    } else {
+        alert("Nhap du lieu !")
     }
-    return total;
+
 }
 // end handel
 // output
-calTotal(x, n);
-console.log(total);
+
+// console.log(total);
 // end output
 /**
  * BAI 3: Nhập vào n. Tính giai thừa 1*2*...n
  */
 // input
-var m = 4;
-var factorial = 1;
+var f = document.getElementById("factorial_input");
+var F = 0;
+var factorial_btn = document.getElementById("factorial_btn");
 // end input
 // handel
-const calFactorial = (m) => {
-    for (let i = 2; i <= m; i++) {
-        factorial *= i;
+factorial_btn.onclick = () => {
+
+    var factorial = 1;
+    F = f.value * 1;
+    if (F > 0) {
+
+        for (let i = 2; i <= F; i++) {
+            factorial *= i;
+        }
+        // output
+        factorial_btn.innerHTML = factorial;
+        // end output
+    } else {
+        alert("Nhập thông tin vào ô !")
     }
-    return factorial;
 }
+
 // end handel
 // output
-console.log(calFactorial(m));
+// console.log(calFactorial(m));
 // end output
 /**
  * BAI 4: Hãy viết chương trình khi click vào button sẽ in ra 10 thẻ div.
@@ -64,45 +127,72 @@ console.log(calFactorial(m));
  */
 // input
 const TOTAL_DIV = 10;
+const total_div_btn = document.getElementById("total_div_btn");
 // end input
 // handel
-const setTotalDiv = (j) => {
-    for (let i = 1; i <= j; i++) {
-        i % 2 === 0 ? console.log(`so chan ` + i) : console.log("so le " + i);;
+total_div_btn.onclick = () => {
+
+ 
+    for (let i = 1; i <= 10; i++) {
+        const element1 = document.createElement("div");
+        const element2 = document.createElement("div");
+        const textNote1 = document.createTextNode("Chẵn")
+        const textNote2 = document.createTextNode("Lẻ")
+        element1.appendChild(textNote1);
+        element2.appendChild(textNote2);
+        element1.setAttribute("style", "background-color:red");
+        element2.setAttribute("style", "background-color:blue; color:white")
+        i % 2 === 0 ? section[3].appendChild(element1) : section[3].appendChild(element2);
     }
+
+
+
 }
 // end handel
 // output
-console.log(setTotalDiv(TOTAL_DIV));
+// console.log(setTotalDiv(TOTAL_DIV));
 // end output
 
 /**
- * BAI 5: Viết chương trình có một ô input, một button. Khi click vào button
+
+* BAI 5: Viết chương trình có một ô input, một button. Khi click vào button
  * thì in ra các số nguyên tố từ 1 tới giá trị của ô input
  */
 // input
-var FACT = "";
-const END = 100;
+
+var fact_input = document.getElementById("fact_input");
+var fact_btn = document.getElementById("fact_btn");
+
+var END = 0;
 
 
 // end input
 // handel
-const calFact = (j) => {
-    for (let i = 2; i <= j; i++) {
+fact_btn.onclick = () => {
+    var FACT = "";
+    END = fact_input.value*1;
+  if (END===0) {
+    alert("Nhập dữ liệu!")
+  } else {
+    for (let i = 2; i <= END; i++) {
         var signal = true;
-      for (let k = 2; k < i; k++) {
-        if (i%k===0) {
-            signal = false;
+        for (let k = 2; k < i; k++) {
+            if (i % k === 0) {
+                signal = false;
+            }
         }
-      }
-      if (signal===true) {
-        
-          FACT += i +",";
-      }
+        if (signal === true) {
+            
+            FACT += i + ",";
+        }
     }
+    fact_btn.innerHTML = `Dãy số nguyên tố: ${FACT}`
+  }
+       
+    
 }
-// end handel
+    // end handel
 // output
-calFact(END);
-console.log(FACT);
+// calFact(END);
+// console.log(FACT);
 // end output
